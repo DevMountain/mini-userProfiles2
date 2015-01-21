43777bf46abd3a0b7ade9f572042d223418b34c2
jacob-israel-turner <poppa.bear.joe@gmail.com>
2015-01-20 22:03:19 -0700

    Reverting to pre-solution.

--- service.js
+++ service.js
@@ -2,11 +2,30 @@ var app = angular.module('userProfiles');
 
 app.service('mainService', function($http) {
 
+	var data = [
+    {
+        "id": 1,
+        "first_name": "george",
+        "last_name": "bluth",
+        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
+    },
+    {
+        "id": 2,
+        "first_name": "lucille",
+        "last_name": "bluth",
+        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
+    },
+    {
+        "id": 3,
+        "first_name": "oscar",
+        "last_name": "bluth",
+        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
+    }
+  ];
 
   this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqr.es/api/users?page=3'
-    })
-  }
+    return data;
+  };
+
+
 });
