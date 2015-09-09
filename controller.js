2404696c6177d1acca822cfef9a3e0529c0badd8
Jeremy R <jeremy@devmounta.in>
2015-09-09 12:25:48 -0600

    with extras

--- controller.js
+++ controller.js
@@ -1,12 +1,27 @@
 var app = angular.module('userProfiles');
 
 app.controller('MainController', function($scope, mainService) {
-
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  
+  $scope.currentPage = 1;
+  $scope.maxPages = 4;
+    
+  $scope.getUsers = function(pageNum) {
+  	$scope.users = mainService.getUsers(pageNum).then(function (response){
+       $scope.users = response.data;  
+       $scope.maxPages = response.total_pages;
+    });
   }
-
-
+  
+  $scope.previous = function(){
+    $scope.currentPage--;   
+    $scope.getUsers($scope.currentPage);
+  }
+  
+  $scope.next = function() {
+    $scope.currentPage++;
+    $scope.getUsers($scope.currentPage);
+  }
+  
   $scope.getUsers();
 
 });
--- service.js
+++ service.js
@@ -2,30 +2,12 @@ var app = angular.module('userProfiles'); //wow
 
 app.service('mainService', function($http) {
 
-	var data = [
-    {
-        "id": 1,
-        "first_name": "george",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
-    },
-    {
-        "id": 2,
-        "first_name": "lucille",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
-    },
-    {
-        "id": 3,
-        "first_name": "oscar",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
-    }
-  ];
-
-  this.getUsers = function() {
-    return data;
+  this.getUsers = function(pageNum) {
+    return $http({
+      method: 'GET',
+      url: 'http://reqr.es/api/users?page=' + pageNum
+    }).then(function(response){
+        return response.data;  
+    });
   };
-
-
 });
