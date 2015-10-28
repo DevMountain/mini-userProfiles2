5e077c488631a4372e5406116230f15274f11439
Jeremy R <jeremy@devmounta.in>
2015-10-28 12:32:09 -0600

    done

--- controller.js
+++ controller.js
@@ -1,11 +1,37 @@
 var app = angular.module('userProfiles');
 
 app.controller('MainController', function($scope, mainService) {
+    
+  var currentPage = 1;
+  var maxPages = 1;
+  var minPages = 1;
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	mainService.getUsers(currentPage)
+    .then(function(response){
+        $scope.users = response.data.data;
+        maxPages = response.data.total_pages;
+    })
   }
+  
+  $scope.isNotAtMax = function(){
+      return currentPage < maxPages;
+  }
+  
 
+  $scope.next = function(){
+      if($scope.isNotAtMax()){
+          currentPage++;
+          $scope.getUsers();
+      }
+  }
+  
+  $scope.previous = function(){
+      if(currentPage > minPages) {
+          currentPage--;
+          $scope.getUsers();
+      }
+  }
 
   $scope.getUsers();
 
