0b3b07531b28622e4b90d438eebf9d00f8f7add8
Jeremy R <jeremy@devmounta.in>
2017-03-29 12:36:50 -0600

    upgrade

--- controller.js
+++ controller.js
@@ -1,10 +1,23 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
+  $scope.currentPage = 1;
+
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
+      var promise = mainService.getUsers($scope.currentPage);
 
-
+      promise.then(function(users){
+          $scope.users = users.data;
+          $scope.total_pages = users.total_pages;
+      })
+  } 
   $scope.getUsers();
 
+  $scope.prev = function(){
+    $scope.currentPage--;
+    $scope.getUsers();
+  }
+  $scope.next = function(){
+    $scope.currentPage++;
+    $scope.getUsers();
+  }
 });
--- service.js
+++ service.js
@@ -1,29 +1,15 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function ($http) {
 
-	var data = [
-    {
-        "id": 0,
-        "first_name": "george",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
-    },
-    {
-        "id": 1,
-        "first_name": "lucille",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
-    },
-    {
-        "id": 2,
-        "first_name": "oscar",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
-    }
-  ];
+    this.getUsers = function (pageNum) {
+        var promise = $http({
+            method: 'GET',
+            url: 'https://reqres.in/api/users?page=' + pageNum
+        })
 
-  this.getUsers = function() {
-    return data;
-  };
-
-
+        return promise.then(function (response) {
+            if (response.status === 200) {
+                return response.data;
+            }
+        })
+    };
 });
